b005ae0bd55926e1693cf420546b8c65ffbc1fa9
Daiwastaken <86420803+Daiwastaken@users.noreply.github.com>
2021-08-26 16:52:00 +0200

    Update clear.js

--- -/commands/clear.js
+++ -/commands/clear.js
@@ -4,36 +4,26 @@ const  { MessageEmbed } = require('discord.js');
 module.exports = {
     name:'clear',
     description: "this is a clear command",
-    aliases: [],
+    aliases: ["purge", "nuke"],
   async execute (message,args, cmd, client, discord) {
     if (!message.member.hasPermission('MANAGE_MESSAGES', { checkAdmin: true, checkOwner: true }) && message.author.id !== config.ownerID) return message.reply('You cannot use this command!')
     if(!message.guild) return;
-    const amount = args[0];
 
-		if (!amount || isNaN(amount))
-			return message.channel.send(new MessageEmbed()
-                .setColor(ee.color)
-                .setAuthor('Please type a real number')
-                .setTimestamp()
-                .setFooter(ee.footertext)
-            )
-		const amountParsed = parseInt(amount);
-		if (amountParsed > 100)
-			return message.channel.send(new MessageEmbed()
-                .setColor(ee.color)
-                .setAuthor('You cannot clear more than 100 messages')
-                .setTimestamp()
-                .setFooter(ee.footertext)
-            );
-		message.channel.bulkDelete(amountParsed + 1);
-		const msg = await message.channel.send(new MessageEmbed()
-            .setColor(ee.color)
-            .setAuthor(`Cleared ${amountParsed} messages`)
-            .setTimestamp()
-            .setFooter(ee.footertext)
-        )
+  // Check if args[0] is a number
+  if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
+      return message.reply("Yeah.... That's not a numer? I also can't delete 0 messages by the way.").then(m => m.delete(5000));
+  }
+  let deleteAmount;
 
-		setTimeout(() => msg.delete(), 800);
+  if (parseInt(args[0]) > 100) {
+      deleteAmount = 100;
+  } else {
+      deleteAmount = parseInt(args[0]);
+  }
+
+  message.channel.bulkDelete(deleteAmount, true)
+      .then(deleted => message.channel.send(`Successfully Deleted ${deleteAmount} Messages.`).then(m => m.delete({timeout: 5000})))
+      .catch(err => message.reply(`Something went wrong... ${err}`));
 	}
 }
   
@@ -42,4 +32,3 @@ module.exports = {
 
 
 // You can only delete the messages which are not older than 14 days.
-
